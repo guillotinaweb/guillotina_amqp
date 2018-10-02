3bceca606dc2cf53c8e46fc20b2d66ba9ca5ea08
Daniel <dmanchon@gmail.com>
2018-10-02 18:29:44 +0200

    Detect hangs on the main loop

--- guillotina_amqp/commands/worker.py
+++ guillotina_amqp/commands/worker.py
@@ -6,18 +6,39 @@ import asyncio
 import logging
 import sys
 import time
+import threading
+import os
 
 
 logger = logging.getLogger('guillotina_amqp')
+logging.basicConfig(level=logging.INFO)
+
+class EventLoopWatchdog(threading.Thread):
+    def __init__(self, loop, timeout):
+        super().__init__()
+        self.loop = loop
+        self.timeout = timeout * 60  # In seconds
+        self._time = loop.time()
+
+    def check(self):
+        _old_time, self._time = self._time, self.loop.time()
+        diff = self._time - _old_time
+        if diff > self.timeout:
+            logger.error(f'Exiting worker because no activity in {diff} seconds')
+            os._exit(1)
+        else:
+            threading.Timer(self.timeout/2, self.check).start()
+
+    def run(self):
+        threading.Timer(10, self.check).start()
 
 
-async def activity_check(worker, timeout):
+# This method just to trigger a context switching in the event loop in
+# case nothing is running. Most likely is not even needed since RMQ/Redis
+# drivers also are running in the same loop.
+async def probe(timeout):
     while True:
-        await asyncio.sleep(60)
-        if (time.time() - worker.last_activity) > timeout:
-            logger.error(f'Exiting worker because no activity in {timeout} seconds')
-            sys.exit(1)
-
+        await asyncio.sleep(timeout)
 
 class WorkerCommand(Command):
     description = 'AMQP worker'
@@ -34,7 +55,11 @@ class WorkerCommand(Command):
         worker = Worker(self.request, self.get_loop())
         await worker.start()
         if arguments.auto_kill_timeout > 0:
-            asyncio.ensure_future(activity_check(worker, arguments.auto_kill_timeout))
+            timeout = arguments.auto_kill_timeout
+            self.get_loop().create_task(probe(timeout/2))
+            t = EventLoopWatchdog(self.get_loop(), timeout)
+            t.start()
+
         while True:
             # make this run forever...
             await asyncio.sleep(999999)
