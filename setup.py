84c09e8341551bb7f3bdb3ebc913a8623f8e7633
vangheem <vangheem@gmail.com>
2018-10-08 12:38:01 -0400

    Back to development: 1.0.8
    Co-authored-by: Wildcard Corp. <corporate@wildcardcorp.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError:
 
 setup(
     name='guillotina_amqp',
-    version='1.0.7',
+    version='1.0.8.dev0',
     description='Integrate amqp into guillotina',
     long_description=README,
     install_requires=[
