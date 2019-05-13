c3b38d1246ada5659c90a3c42d35c779c5f48ebe
vangheem <vangheem@gmail.com>
2019-05-13 14:45:34 -0400

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError:
 
 setup(
     name='guillotina_amqp',
-    version='3.0.0',
+    version='3.0.1.dev0',
     description='Integrate amqp into guillotina',
     long_description=README,
     install_requires=[
