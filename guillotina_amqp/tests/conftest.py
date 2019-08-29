9f9c97c3df7b7d901a3e6a0c34d1c78022f9e0fe
vangheem <vangheem@gmail.com>
2019-08-29 14:16:44 -0400

    fix

--- guillotina_amqp/tests/conftest.py
+++ guillotina_amqp/tests/conftest.py
@@ -1,5 +1,5 @@
 pytest_plugins = [
-    # 'aiohttp.pytest_plugin',
+    'aiohttp.pytest_plugin',
     'guillotina.tests.fixtures',
     'guillotina_amqp.tests.fixtures',
     'pytest_docker_fixtures',
