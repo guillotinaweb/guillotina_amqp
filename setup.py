abe14c95d4fa534fd316cbd4a6caa7fdb3a803a8
vangheem <vangheem@gmail.com>
2019-11-05 16:07:39 -0500

    Preparing release 5.0.10

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError:
 
 setup(
     name='guillotina_amqp',
-    version='5.0.10.dev0',
+    version='5.0.10',
     description='Integrate amqp into guillotina',
     long_description=README,
     install_requires=[
