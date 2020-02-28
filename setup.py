f878291f0bc3823de7da3a2bfb6bdb85e03adb46
vangheem <vangheem@gmail.com>
2020-02-28 09:08:22 -0500

    Back to development: 5.0.19

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError:
 
 setup(
     name="guillotina_amqp",
-    version="5.0.18",
+    version="5.0.19.dev0",
     description="Integrate amqp into guillotina",
     long_description=README,
     install_requires=[
