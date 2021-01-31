be79d4357c3245b7ffc091d7ca6de97f9f91897a
vangheem <vangheem@gmail.com>
2021-01-31 11:48:19 -0500

    fix

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
             "pytest>=3.8.0<=5.0.0",
             "docker",
             "psycopg2-binary",
-            "pytest-asyncio==0.10.0",
+            "pytest-asyncio>=0.12.0",
             "pytest-cov>=2.6.1",
             "coverage>=4.4",
             "pytest-docker-fixtures[rabbitmq]==1.3.5",
